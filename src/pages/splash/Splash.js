c2fce3511be48a5948ab2148b443bdf02a8ff1ea
Mohit Tejwani <mohittejwani@LMOLMACLTGCCQV94.local>
2025-01-14 23:22:45 +0530

    remove animaed

--- src/pages/splash/Splash.js
+++ src/pages/splash/Splash.js
@@ -1,35 +1,33 @@
 import React, { Component } from "react";
 import "./Splash.css";
 import { Redirect } from "react-router-dom";
-import LoaderLogo from "../../components/Loader/LoaderLogo.js";
+// import LoaderLogo from "../../components/Loader/LoaderLogo.js";
 
-function AnimatedSplash(props) {
-  console.log(props.theme, "props.theme");
+// function AnimatedSplash(props) {
+//   console.log(props.theme, "props.theme");
 
-  return (
-    <div className="logo_wrapper">
-      <div className="screen" style={{ backgroundColor: props.theme.splashBg }}>
-        <LoaderLogo id="logo" theme={props.theme} />
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="logo_wrapper">
+//       <div className="screen" style={{ backgroundColor: props.theme.splashBg }}>
+//         <LoaderLogo id="logo" theme={props.theme} />
+//       </div>
+//     </div>
+//   );
+// }
 
 class Splash extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      redirect: false,
-    };
+    this.state = {};
   }
 
-  componentDidMount() {
-    this.id = setTimeout(() => this.setState({ redirect: true }), 5500);
-  }
+  // componentDidMount() {
+  //   this.id = setTimeout(() => this.setState({ redirect: true }), 5500);
+  // }
 
-  componentWillMount() {
-    clearTimeout(this.id);
-  }
+  // componentWillMount() {
+  //   clearTimeout(this.id);
+  // }
 
   // render() {
   //   return this.state.redirect ? (
